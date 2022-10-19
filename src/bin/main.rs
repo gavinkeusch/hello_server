4a2906469bdc6d5620a9645d996771208868644f
gavin <gavinkeusch@gmail.com>
2022-10-18 18:40:17 -0700

    finished chapter

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+use hello_server::ThreadPool;
 
-    for stream in listener.incoming() {
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -27,7 +34,9 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line, contents.len(), contents
+        status_line,
+        contents.len(),
+        contents
     );
 
     stream.write(response.as_bytes()).unwrap();
